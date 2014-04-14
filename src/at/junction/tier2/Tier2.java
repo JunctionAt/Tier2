92827d663c90d2dd74561b8f1f93b7fcbca9d868
Tom Powell <zifnab@zifnab06.net>
2014-04-13 22:05:51 -0600

    Add 'tier2.ticket.all' permission - allows player with permission to see all tickets, even elevated ones (with elevation status)

--- src/at/junction/tier2/Tier2.java
+++ src/at/junction/tier2/Tier2.java
@@ -689,7 +689,8 @@ public class Tier2 extends JavaPlugin {
             // Check that it's either unelevated or they have the appropriate permissions.
             if (ticket.getStatus() != TicketStatus.ELEVATED
                     || perms.isInGroup(player, ticket.getElevationGroup())
-                    || perms.isInGroup(player, config.GROUPPREFIX + ticket.getElevationGroup())) {
+                    || perms.isInGroup(player, config.GROUPPREFIX + ticket.getElevationGroup())
+                    || player.hasPermission("tier2.ticket.all")) {
                 player.sendMessage(ChatColor.DARK_AQUA + "#" + ticket.getId() + " by " + ticket.getPlayerName() + ":");
                 String messageBody = ticket.getTicket();
                 if (ticket.getTicket().length() > 25) {
